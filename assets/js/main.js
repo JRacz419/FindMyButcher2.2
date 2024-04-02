88fa4c6172dff9f87e28ce6db05ed8cd7a48d5a0
Josh <jracz419@gmail.com>
2024-04-02 10:13:59 -0400

    linked ohio.html to index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,88 +1,3 @@
-/*=============== SHOW MENU ===============*/
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
-}
-
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
-}
-
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*=============== ADD BLUR TO HEADER ===============*/
-const blurHeader = () =>{
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('blur-header') 
-                       : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
-
-/*=============== SHOW SCROLL UP ===============*/ 
-const scrollUp = () =>{
-	const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
-						: scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp)
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
-    
-const scrollActive = () =>{
-  	const scrollY = window.pageYOffset
-
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
-
-		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
-}
-window.addEventListener('scroll', scrollActive)
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 3000,
-    delay: 400,
-    // reset: true // Animations repeat
-})
-
-sr.reveal(`.home__data, .explore__data, .explore__user, .footer__container`)
-sr.reveal(`.home__card`, {delay: 600, distance: '100px', interval: 100})
-sr.reveal(`.about__data, .join__image`, {origin: 'right'})
-sr.reveal(`.about__image, .join__data`, {origin: 'left'})
-sr.reveal(`.popular__card`, {interval: 200})
-
 const wrapper = document.querySelector(".wrapper"); 
 const selectBtn = wrapper.querySelector(".select-btn"); 
 const searchInp = wrapper.querySelector("input"); 
@@ -95,7 +10,7 @@ let countries = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colo
 "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont","Virginia",
 "Washington", "West Virginia","Wisconsin", "Wyoming"];  
 
-let countryLinks = {   /* "Ohio": "assets/states/ohio/ohio.html"*/    };  
+let countryLinks = {    "Ohio": "assets/states/ohio/ohio.html",    };   
 
 function addCountry(selectedCountry = null) {     options.innerHTML = "";     
 
@@ -122,8 +37,7 @@ countries.forEach(country => {
         
         selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
 
-        
-/*cards*/
+        /*cards*/
 
 
 const observer = new IntersectionObserver((entries) =>  {
