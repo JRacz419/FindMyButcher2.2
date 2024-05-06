35a79f18d9b5765667cb2053ab21c2e7b68ecf19
Josh <jracz419@gmail.com>
2024-05-06 07:46:52 -0400

    Created state of indiana Added heeader Added cities and butcher shops

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ let countries = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colo
 
  
 let countryLinks = {   "Ohio": "./assets/states/ohio/ohio.html", "Michigan": "./assets/states/michigan/michigan.html",
-"Kentucky": "./assets/states/kentucky/kentucky.html",
+"Kentucky": "./assets/states/kentucky/kentucky.html", "Indiana": "./assets/states/indiana/indiana.html",
 };  
 
 function addCountry(selectedCountry = null) {     options.innerHTML = "";     
